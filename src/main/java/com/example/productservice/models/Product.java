d8c0189a23a361f2027f30911b3f53304d01c707
Chethan C K <chethanckm66@gmail.com>
2025-03-22 20:26:54 +0530

    Database connection

--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,12 +1,17 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 @Data
-public class Product {
-    private Long id;
+@Setter
+@Entity
+public class Product extends BaseModel {
     private String title;
     private double price;
+    @ManyToOne
     private Category category;
     private String description;
     private String imageUrl;
