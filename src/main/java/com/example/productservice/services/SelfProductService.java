226ee41ee743134f8ee2055f7c027b573aeac378
Chethan C K <chethanckm66@gmail.com>
2025-03-25 23:50:25 +0530

    Adding product and category

--- src/main/java/com/example/productservice/services/SelfProductService.java
+++ src/main/java/com/example/productservice/services/SelfProductService.java
@@ -1,7 +1,9 @@
 package com.example.productservice.services;
 
 import com.example.productservice.exceptions.ProductNotExistsException;
+import com.example.productservice.models.Category;
 import com.example.productservice.models.Product;
+import com.example.productservice.repositories.CategoryRepository;
 import com.example.productservice.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
@@ -14,9 +16,13 @@ import java.util.Optional;
 //@Primary -> This will also works but easy to make mistake if someone make other class as Primary
 public class SelfProductService  implements ProductService {
     private ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
     @Autowired
-    public SelfProductService(ProductRepository productRepository) {
+    public SelfProductService(ProductRepository productRepository,
+                              CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
@@ -35,7 +41,14 @@ public class SelfProductService  implements ProductService {
 
     @Override
     public Product saveProduct(Product product) {
-
+        Category category = product.getCategory();
+        Optional<Category> categoryOptional = categoryRepository.findByName(category.getName());
+        if (categoryOptional.isEmpty()) {
+            Category newCategory = categoryRepository.save(category);
+            product.setCategory(newCategory);
+        } else {
+            product.setCategory(categoryOptional.get());
+        }
         return productRepository.save(product);
     }
 
